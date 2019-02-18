1db2844eeaa167fbf060fc571ef97341c6abcee3
Scott <sderyck@gmail.com>
2019-02-17 20:42:30 -0500

    commit message

---
+++ e.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+ 
+ 
+fromaddr = "jvd330@gmail.com"
+toaddr = "sderyck@gmail.com"
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "FROM THE GUYS"
+ 
+body = "CURRENT TEMP IS 75F"
+msg.attach(MIMEText(body, 'plain'))
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "J123456!")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
